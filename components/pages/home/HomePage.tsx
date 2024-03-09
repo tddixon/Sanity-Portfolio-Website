6721b54e1c07ce5188d27c08af136cc5568ef1eb
judahnarrowstack <judah@narrowstack.com>
2024-03-08 19:48:29 -0600

    fixing build error

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -1,7 +1,7 @@
 import type { EncodeDataAttributeCallback } from '@sanity/react-loader'
 import Link from 'next/link'
 
-import { ProjectListItem } from '@/components/pages/home/ProjectListItem'
+// import { ProjectListItem } from '@/components/pages/home/ProjectListItem'
 import { Header } from '@/components/shared/Header'
 import { resolveHref } from '@/sanity/lib/utils'
 import type { HomePagePayload } from '@/types'
@@ -23,29 +23,29 @@ export function HomePage({ data, encodeDataAttribute }: HomePageProps) {
       {/* Header */}
       {title && <Header title={title} subtitle={subtitle} description={overview} />}
       {/* Showcase projects */}
-      {showcaseProjects && showcaseProjects.length > 0 && (
-        <div className="mx-auto max-w-[100rem] rounded-md border">
-          {showcaseProjects.map((project, key) => {
-            const href = resolveHref(project?._type, project?.slug)
-            if (!href) {
-              return null
-            }
-            return (
-              <Link
-                key={key}
-                href={href}
-                data-sanity={encodeDataAttribute?.([
-                  'showcaseProjects',
-                  key,
-                  'slug',
-                ])}
-              >
-                <ProjectListItem project={project} odd={key % 2} />
-              </Link>
-            )
-          })}
-        </div>
-      )}
+      {/* {showcaseProjects && showcaseProjects.length > 0 && ( */}
+      {/*   <div className="mx-auto max-w-[100rem] rounded-md border"> */}
+      {/*     {showcaseProjects.map((project, key) => { */}
+      {/*       const href = resolveHref(project?._type, project?.slug) */}
+      {/*       if (!href) { */}
+      {/*         return null */}
+      {/*       } */}
+      {/*       return ( */}
+      {/*         <Link */}
+      {/*           key={key} */}
+      {/*           href={href} */}
+      {/*           data-sanity={encodeDataAttribute?.([ */}
+      {/*             'showcaseProjects', */}
+      {/*             key, */}
+      {/*             'slug', */}
+      {/*           ])} */}
+      {/*         > */}
+      {/*           <ProjectListItem project={project} odd={key % 2} /> */}
+      {/*         </Link> */}
+      {/*       ) */}
+      {/*     })} */}
+      {/*   </div> */}
+      {/* )} */}
       <SectionParallax products={products} />
     </div>
   )
